0a6047b2f6f420fc6383ce084caa07b3371cdf31
Eric Fiselier <eric@efcs.ca>
2015-03-10 22:07:10 -0400

    stuff

--- test/catch_pointer_nullptr.pass.cpp
+++ test/catch_pointer_nullptr.pass.cpp
@@ -109,7 +109,7 @@ int main()
    catch_test<int A::*, int A::*>();
    //catch_test<int A::*, int A::* const>();
    //catch_test<int A::*, int A::* volatile>();
-   
+
    //catch_test<int A::*, const int A::*>();
    catch_test<int A::*, const int A::* const>();
    catch_test<int A::*, const int A::* volatile>();
@@ -117,14 +117,14 @@ int main()
    catch_test<int A::*, volatile int A::* volatile>();
    catch_test<int A::*, volatile int A::* const>();
 
-/*
+
    catch_test<int A::**, int A::**>();
    catch_test<int A::**, int A::* const *>();
    catch_test<int A::**, int A::** const>();
    catch_test<int A::**, int A::* const* const>();
-    
+
     catch_test<int A::**, const int A::* const *>();
     //catch_test<int A::**, const int A::** const>();
     catch_test<int A::**, const int A::* const* const>();
-    */
+
 }
