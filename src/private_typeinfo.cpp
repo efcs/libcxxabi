d9b15f41a40ebb8f0a76ff094b3b78bf162c2d48
Eric Fiselier <eric@efcs.ca>
2015-03-10 21:24:08 -0400

    stuff

--- src/private_typeinfo.cpp
+++ src/private_typeinfo.cpp
@@ -410,16 +410,16 @@ __pointer_type_info::can_catch(const __shim_type_info* thrown_type,
         if (member_ptr_type->can_catch(thrown_member_ptr_type, adjustedPtr))
             return true;
         
-        if (is_equal(member_ptr_type->__context, thrown_member_ptr_type->__context, false)) {
-           
+        if (!is_equal(member_ptr_type->__context, thrown_member_ptr_type->__context, false))
+           return false;
+        
         if (!is_equal(member_ptr_type->__pointee, thrown_member_ptr_type->__pointee, false))
             return false;
+        
         if (adjustedPtr != NULL)
             adjustedPtr = *static_cast<void**>(adjustedPtr);
         
-        return true;
-        }
-        return false;
+       return true;
     }
 
     // Handle pointer to class type
