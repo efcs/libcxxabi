b80bb42a9ab4903ef30ed015a27a67d3801e2236
Eric Fiselier <eric@efcs.ca>
2015-03-10 21:41:33 -0400

    stuff

--- test/catch_pointer_nullptr.pass.cpp
+++ test/catch_pointer_nullptr.pass.cpp
@@ -57,8 +57,63 @@ void test2()
 
 #endif
 
+template <typename T, typename C>
+void catch_test() {
+#if __has_feature(cxx_nullptr)
+   try { throw (T) nullptr; assert(false); }
+   catch (C) {}
+   catch (...) { assert(false); }
+#endif
+
+   try { throw (T) NULL; assert(false); }
+   catch (C) {}
+   catch (...) { assert(false); }
+   }
+
 int main()
 {
+// catch naked nullptrs
     test1();
     test2();
+
+   catch_test<int **, int **>();                       // pointer to pointer to int
+
+   catch_test<int **, int * const *>();                // pointer to const pointer to int
+   catch_test<int **, int ** const>();                 // const pointer to pointer to int
+   catch_test<int **, int * const * const>();          // const pointer to const pointer to int
+   catch_test<int **, const int **>();                 // pointer to pointer to const int
+   catch_test<int **, const int * const *>();          // pointer to const pointer to const int
+   catch_test<int **, const int ** const>();           // const pointer to pointer to const int
+   catch_test<int **, const int * const * const>();    // const pointer to const pointer to const int
+
+   catch_test<int **, int * volatile *>();                     // pointer to volatile pointer to int
+   catch_test<int **, int ** volatile>();                      // volatile pointer to pointer to int
+   catch_test<int **, int * volatile * volatile>();            // volatile pointer to volatile pointer to int
+   catch_test<int **, volatile int **>();                      // pointer to pointer to volatile int
+   catch_test<int **, volatile int * volatile *>();            // pointer to volatile pointer to volatile int
+   catch_test<int **, volatile int ** volatile>();             // volatile pointer to pointer to volatile int
+   catch_test<int **, volatile int * volatile * volatile>();   // volatile pointer to volatile pointer to volatile int
+
+
+// Member functions
+   catch_test<int A::*, int A::*>();
+   catch_test<int A::*, int A::* const>();
+   catch_test<int A::*, int A::* volatile>();
+
+   catch_test<int A::*, const int A::*>();
+   catch_test<int A::*, const int A::* const>();
+   catch_test<int A::*, const int A::* volatile>();
+   catch_test<int A::*, volatile int A::*>();
+   catch_test<int A::*, volatile int A::* volatile>();
+   catch_test<int A::*, volatile int A::* const>();
+
+
+   catch_test<int A::**, int A::**>();
+   catch_test<int A::**, int A::* const *>();
+   catch_test<int A::**, int A::** const>();
+   catch_test<int A::**, int A::* const* const>();
+    
+    catch_test<int A::**, const int A::* const *>();
+    catch_test<int A::**, const int A::** const>();
+    catch_test<int A::**, const int A::* const* const>();
 }
