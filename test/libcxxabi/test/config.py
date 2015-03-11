778b828eff1d659851b5714083ca275f60568ac5
Eric Fiselier <eric@efcs.ca>
2015-03-10 20:25:09 -0400

    revert stuff

--- test/libcxxabi/test/config.py
+++ test/libcxxabi/test/config.py
@@ -61,8 +61,6 @@ class Configuration(LibcxxConfiguration):
         pass
 
     def configure_link_flags_abi_library_path(self):
-        cxx_path = '/usr/local/google/home/ericwf/workspace/build-libcxx/lib'
-        self.cxx.link_flags += ['-L' + cxx_path, '-Wl,-rpath,' + cxx_path]
         # Configure ABI library paths.
         if self.libcxxabi_lib_root:
             self.cxx.link_flags += ['-L' + self.libcxxabi_lib_root,
