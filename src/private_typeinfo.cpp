ff2eb0d3fd63daa15959a027c2c4850922ea3186
Eric Fiselier <eric@efcs.ca>
2015-03-10 21:17:57 -0400

    stuff

--- src/private_typeinfo.cpp
+++ src/private_typeinfo.cpp
@@ -411,7 +411,7 @@ __pointer_type_info::can_catch(const __shim_type_info* thrown_type,
             return false;
         if (!is_equal(member_ptr_type->__context, thrown_member_ptr_type->__context, false))
             return false;
-        if (!is_equal(member_ptr_type->__context->__pointee, thrown_member_ptr_type->__context->__pointee, false))
+        if (!is_equal(member_ptr_type->__pointee, thrown_member_ptr_type->__pointee, false))
             return false;
         if (adjustedPtr != NULL)
             adjustedPtr = *static_cast<void**>(adjustedPtr);
